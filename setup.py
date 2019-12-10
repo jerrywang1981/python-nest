9de4f8c23b80ade3333f7cdd3e8fe4e1fe3912ec
Jerry Wang <jianjunw@cn.ibm.com>
2019-12-10 22:27:25 +0800

    0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-nestjs", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Jerry Wang",
     author_email="wangjianjun@gmail.com",
     description="The library for nest style microservice",
