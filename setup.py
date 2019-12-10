3fe8b376cae54084ad7f3bf9289adf8bbd0c3ede
Jerry Wang <jianjunw@cn.ibm.com>
2019-12-10 19:55:32 +0800

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-rest", # Replace with your own username
+    name="python-nestjs", # Replace with your own username
     version="0.0.1",
     author="Jerry Wang",
     author_email="wangjianjun@gmail.com",
